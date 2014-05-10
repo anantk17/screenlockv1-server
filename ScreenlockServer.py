d5bacced5b7a235f849a096e9b09c1a33e2a09ad
Anant <anantk17@ymail.com>
2014-05-10 20:47:35 +0530

    Added UI elements and form validation

--- ScreenlockServer.py
+++ ScreenlockServer.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import re
 import os
+import pickle
 from PySide import QtGui,QtCore
 from KThread import *
 from IP import *
@@ -9,19 +10,33 @@ from IP import *
 class MainScreen(QtGui.QWidget):
 
     def __init__(self):
-        self.OS = ""
-        #self.host = '10.0.0.6'
+        self.store ={}
+        ### Load persistent data from file 
+        try:
+            self.store = pickle.load(open(".screenlock.p","rb"))
+                   
+        ### If pickle file is not present, create a new pickle dump file
+        except IOError:
+            self.store = {"OS":"", "MAC":"","Name":""}
+            self.f = open(".screenlock.p","wb")
+            pickle.dump( self.store,f)
+        
+        self.OS = self.store["OS"]
+        self.regMAC = self.store["MAC"]
+        self.regName = self.store["Name"]
+        
+        ###Set host ip and port
         self.host = get_ip()
-        self.port = 8082 
+        self.port = 8082
+
+        ###Running status for the network thread
         self.running = False
-        self.regMAC = ""
-        self.regName =  ""
+        
         super(MainScreen,self).__init__()
         self.initUI()
 
     def initUI(self):
         
-        #print "HELLO"
         start = QtGui.QPushButton("Start")
         start.clicked.connect(self.sock_start)
         stop = QtGui.QPushButton("Stop")
@@ -30,17 +45,26 @@ class MainScreen(QtGui.QWidget):
         self.label2 = QtGui.QLabel('Authorised Device')
         self.label3 = QtGui.QLabel('Device Name')
         self.label4 = QtGui.QLabel('MAC Address')
+        self.edit = QtGui.QPushButton("Edit")
+        self.edit.clicked.connect(self.butchange1)
         self.textfield1 = QtGui.QLineEdit()
+        self.textfield1.setText(self.regName)
+        self.textfield1.setReadOnly(True)
         self.textfield1.textChanged[str].connect(self.onChange1)
         self.textfield2 = QtGui.QLineEdit()
+        self.textfield2.setText(self.regMAC)
+        self.textfield2.setReadOnly(True)
         self.textfield2.textChanged[str].connect(self.onChange2)
-
+        self.textfield2.setInputMask("HH:HH:HH:HH:HH:HH")
+        
         combo = QtGui.QComboBox()
-        combo.addItem("Select OS")
-        combo.addItem("Ubuntu")
-        combo.addItem("Xubuntu")
-        combo.addItem("Kubuntu")
-        combo.addItem("Windows")
+        oslist = ["Select OS", "Ubuntu", "Xubuntu", "Kubuntu", "Windows"]
+        combo.addItems(oslist)
+
+        if self.OS == "":
+            combo.setCurrentIndex(0)
+        else:
+            combo.setCurrentIndex(oslist.index(self.OS))
         
         combo.activated[str].connect(self.onActivated)
 
@@ -50,6 +74,7 @@ class MainScreen(QtGui.QWidget):
         grid.addWidget(start,0,0)
         grid.addWidget(stop,0,3)
         grid.addWidget(self.label2,1,0)
+        grid.addWidget(self.edit,1,2)
         grid.addWidget(self.label3,2,0)
         grid.addWidget(self.textfield1,2,2)
         grid.addWidget(self.label4,3,0)
@@ -64,14 +89,26 @@ class MainScreen(QtGui.QWidget):
 
     def onActivated(self,text):
         self. OS = text
-        #self.label1.setText(text)
-        #self.label1.adjustSize()
 
     def onChange1(self, text):
         self.regName =  text
 
     def onChange2(self, text):
         self.regMAC = text
+
+    def butchange1(self):
+        self.edit.setText("Save")
+        self.edit.clicked.connect(self.butchange2)
+        #self.editreg()
+        self.textfield1.setReadOnly(False)
+        self.textfield2.setReadOnly(False)
+    
+    def butchange2(self):
+        self.edit.setText("Edit")
+        self.edit.clicked.connect(self.butchange1)
+        #self.editreg()
+        self.textfield1.setReadOnly(True)
+        self.textfield2.setReadOnly(True)
 
     def net_op(self,sock):
         if(self.running):
@@ -144,6 +181,10 @@ class MainScreen(QtGui.QWidget):
             self.t.kill()
             self.sock.shutdown(socket.SHUT_RDWR)
             self.sock.close()
+        self.store["OS"] = self.OS
+        self.store["MAC"] = self.regMAC
+        self.store["Name"] = self.regName
+        pickle.dump(self.store,open(".screenlock.p","wb"))
 
         event.accept()
 
